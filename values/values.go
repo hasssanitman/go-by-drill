f5c58bdd5683aab6d38bfe773587114e20b68d36
Hassan Mohammadian <hasssanitman@gmail.com>
2023-05-14 10:38:20 +0330

    feat(values): add values topic

---
+++ values/values.go
@@ -0,0 +1,31 @@
+// Go has various value types including strings, integers, floats, booleans, etc. Here are a few basic examples.
+
+// Strings, which can be added together with +.
+
+// Integers and floats.
+
+// Booleans, with boolean operators as you’d expect.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go by " + "DRILLL!")
+	fmt.Println("100 + 200 = ", 100+200)
+	fmt.Println("7.0/ 3.0 = ", 7.0/3.0)
+	fmt.Println("10*10 = ", 10*10)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+// $ go run main.go
+// go by DRILLL!
+// 100 + 200 =  300
+// 7.0/ 3.0 =  2.3333333333333335
+// 10*10 =  100
+// false
+// true
+// false
