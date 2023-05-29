9a4148f58eb98c5e65caa33b73f1bee95cd42a07
Hassan Mohammadian <hasssanitman@gmail.com>
2023-05-29 17:24:40 +0330

    feat: Add random numbers

---
+++ random_numbers/random_numbers.go
@@ -0,0 +1,69 @@
+// Go’s math/rand package provides pseudorandom number
+// generation.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	// For example, rand.Intn returns a random int n, 0 <= n <
+	// 100.
+	fmt.Print(rand.Intn(100), ",")
+	fmt.Println(rand.Intn(100))
+
+	// rand.Float64 returns a float64 f, 0.0 <= f < 1.0.
+	fmt.Println(rand.Float64())
+
+	// This can be used to generate random floats in other ranges, for
+	// example 5.0 <= f' < 10.0.
+	fmt.Print((rand.Float64()*5)+5, ",")
+	fmt.Print((rand.Float64() * 5) + 5)
+	fmt.Println()
+
+	// The default number generator is deterministic, so it’ll produce
+	// the same sequence of numbers each time by default. To produce
+	// varying sequences, give it a seed that changes. Note that this is
+	// not safe to use for random numbers you intend to be secret; use
+	// crypto/rand for those.
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	// Call the resulting rand.Rand just like the functions on the rand
+	// package.
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Println(r1.Intn(100))
+
+	// If you seed a source with the same number, it produces the same
+	// sequence of random numbers.
+	s2 := rand.NewSource(42)
+	r2 := rand.New(s2)
+	fmt.Print(r2.Intn(100), ",")
+	fmt.Println(r2.Intn(100))
+
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	fmt.Print(r3.Intn(100), ",")
+	fmt.Println(r3.Intn(100))
+
+}
+
+// Depending on where you run this sample, some of the generated
+// numbers may be different. Note that on the Go playground
+// seeding with time.Now() still produces deterministic results
+// due to the way the playground is implemented.
+
+// $ go run random_numbers/random_numbers.go
+// 4,3
+// 0.21048169808557254
+// 8.828417414321803,7.516985617873744
+// 48,24
+// 5,87
+// 5,87
+
+// See the math/rand package docs for references on other
+// random quantities that Go can provide.
